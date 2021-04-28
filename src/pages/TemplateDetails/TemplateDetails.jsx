20cecb8bb17455ffcaf68942dd363ac1536f8a3c
Rom Soloman <romsoloman19@gmail.com>
2021-04-28 10:25:41 +0300

    add details page basic

--- src/pages/TemplateDetails/TemplateDetails.jsx
+++ src/pages/TemplateDetails/TemplateDetails.jsx
@@ -1,18 +1,17 @@
-
-
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { getTemplateById } from '../../store/actions/templateActions';
+import { useSelector, useDispatch } from 'react-redux';
 import './TemplateDetails.scss'
 
 export const TemplateDetails = (props) => {
-    const [currTemplate, setCurrTemplate] = useState(null);
+    const currTemplate = useSelector(state => state.templateReducer.currTemplate)
+    const dispatch = useDispatch();
     useEffect(() => {
-        setCurrTemplate(prevState => prevState = getTemplateById(props.match.params.id))
-    })
+        dispatch(getTemplateById(props.match.params.id));
+    }, [])
     return (
-        <div>
-
-        </div>
+        currTemplate && <section className='details-page'>
+        </section>
     )
 }
 
