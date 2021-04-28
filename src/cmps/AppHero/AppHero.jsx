80b81fb6b17a7095dbb4484d64afa514e4bd8915
Rom Soloman <romsoloman19@gmail.com>
2021-04-28 16:53:38 +0300

    add htmls to sidebar

--- src/cmps/AppHero/AppHero.jsx
+++ src/cmps/AppHero/AppHero.jsx
@@ -6,9 +6,9 @@ export const AppHero = (props) => {
 
     return (
         <section className='container app-hero'>
-            <div className="title">
+            <div className="flex column align-center justify-center text-center title">
                 <h1>welcome to wixin, your free website builder</h1>
-                <h3>in our app, you can create an amazing app just from scratch!</h3>
+                <h3>In our app, you can create an amazing app just from scratch!</h3>
             </div>
             <div className="video-example">
                 {/* <video src=""></video> */}
