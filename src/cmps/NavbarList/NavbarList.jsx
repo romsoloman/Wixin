f1adab9b21015d74dc5df19820dd981b99bd003b
odedron100 <odedron84@gmail.com>
2021-04-28 11:22:05 +0300

    Fix navbar controller

--- src/cmps/NavbarList/NavbarList.jsx
+++ src/cmps/NavbarList/NavbarList.jsx
@@ -1,13 +1,7 @@
-import React, { useEffect } from 'react';
 import { NavbarPreview } from '../NavbarPreview/NavbarPreview'
 import './NavbarList.scss'
 
 export const NavbarList = ({props}) => {
-    useEffect(() => {
-        console.log('props',props );
-        return () => {
-        }
-    }, [])
     return (
        props && <ul className="items-list-controller">
             {props.data.map((navbar) => <NavbarPreview navbar={navbar} key={navbar._id}/>)}
--- src/cmps/NavbarPreview/NavbarPreview.jsx
+++ src/cmps/NavbarPreview/NavbarPreview.jsx
@@ -1,20 +1,9 @@
-import React, { useEffect } from 'react';
 import './NavbarPreview.scss'
 
 export const NavbarPreview = ({navbar}) => {
-   useEffect(() => {
-        console.log('navbar',navbar );
-        return () => {
-        }
-    }, [])
     return (
        navbar && <section className="item-controller">
-            <div>{navbar.logo}</div>
-            <div>
-                {navbar.a.map((aItem) =>
-                    <a href="#">{aItem}</a>
-                )}
-            </div>
+           <img src={navbar.imgUrl} alt=""/>
         </section>
     )
 }
