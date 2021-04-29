86e623e87895935ad225531d54d83106ea1f7a04
Rom Soloman <romsoloman19@gmail.com>
2021-04-29 16:08:18 +0300

    add details page basic

--- src/pages/TemplateDetails/TemplateDetails.jsx
+++ src/pages/TemplateDetails/TemplateDetails.jsx
@@ -12,6 +12,34 @@ export const TemplateDetails = (props) => {
     }, [])
     return (
         currTemplate && <section className='editor-container details-page'>
+            <header>
+                <nav>
+                    <h1>{currTemplate.addOns.header.navbar.logo}</h1>
+                    <div>
+                        {currTemplate.addOns.header.navbar.a.map(item => <a href="#">{item}</a>)}
+                    </div>
+                </nav>
+                <section>
+                    <h1>{currTemplate.addOns.header.hero.h1}</h1>
+                    <p>{currTemplate.addOns.header.hero.p}</p>
+                    <button>{currTemplate.addOns.header.hero.button}</button>
+                </section>
+            </header>
+            <main>
+                {currTemplate.addOns.main.sections.map(section => {
+                    return (
+                        <div>
+                            <img src={section.img} alt="" />
+                            <h1>{section.h1}</h1>
+                            <p>{section.p}</p>
+                        </div>
+                    )
+                })}
+            </main>
+            <footer>
+                <h3>{currTemplate.addOns.footer.h3}</h3>
+                {currTemplate.addOns.footer.a.map(item => <a href='#'>{item}</a>)}
+            </footer>
         </section>
     )
 }
