bee23a8829ed4c2f2ebcb2335a74440abd803fa2
Rom Soloman <romsoloman19@gmail.com>
2021-05-02 12:43:47 +0300

    fix controller add

--- src/cmps/ControllerAdd/ControllerAdd.jsx
+++ src/cmps/ControllerAdd/ControllerAdd.jsx
@@ -2,24 +2,24 @@ import React, { useEffect } from 'react';
 import { loadHtmlTags } from '../../store/actions/htmlTagsAction';
 import { useDispatch, useSelector } from 'react-redux';
 import './ControllerAdd.scss'
-import {NavbarList} from '../NavbarList/NavbarList';
-import {HeroList} from '../HeroList/HeroList';
-import {SectionList} from '../SectionList/SectionList';
-import {CardList} from '../CardList/CardList';
-import {TextList} from '../TextList/TextList';
-import {ImageList} from '../ImageList/ImageList';
-import {FormList} from '../FormList/FormList';
-import {FooterList} from '../FooterList/FooterList';
+import { NavbarList } from '../NavbarList/NavbarList';
+import { HeroList } from '../HeroList/HeroList';
+import { SectionList } from '../SectionList/SectionList';
+import { CardList } from '../CardList/CardList';
+import { TextList } from '../TextList/TextList';
+import { ImageList } from '../ImageList/ImageList';
+import { FormList } from '../FormList/FormList';
+import { FooterList } from '../FooterList/FooterList';
 
 const cmpMap = {
     'Navbars': NavbarList,
     'Heroes': HeroList,
-    'Sections':SectionList,
-    'Cards':CardList,
-    'Texts':TextList,
-    'Images':ImageList,
-    'Forms':FormList,
-    'Footers':FooterList,
+    'Sections': SectionList,
+    'Cards': CardList,
+    'Texts': TextList,
+    'Images': ImageList,
+    'Forms': FormList,
+    'Footers': FooterList,
 
 }
 
