cdd2a7f5c2d00739a965b910088ebb4e3e76c3fb
odedron100 <odedron84@gmail.com>
2021-05-04 14:49:18 +0300

    Fix add controller

--- src/cmps/NavbarList/NavbarList.jsx
+++ src/cmps/NavbarList/NavbarList.jsx
@@ -4,6 +4,7 @@ import './NavbarList.scss'
 export const NavbarList = ({ props, getCmp,templateId }) => {
     return (
         props && <section>
+            NAVBAR
             <ul className="items-list-controller">
                 {props.data.map((navbar) => <NavbarPreview navbar={navbar} key={navbar.id} templateId={templateId} getCmp={getCmp} tagName={props.tagName} />)}
             </ul>
--- src/cmps/SectionList/SectionList.jsx
+++ src/cmps/SectionList/SectionList.jsx
@@ -5,6 +5,7 @@ export const SectionList = ({ props, getCmp, templateId }) => {
 
     return (
         props && <section>
+            SECTION
             <ul className="items-list-controller">
                 {props.data.map((section) => <SectionPreview section={section} key={section.id}  templateId={templateId} getCmp={getCmp} tagName={props.tagName} />)}
             </ul>
